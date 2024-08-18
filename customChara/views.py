b9fc0d9c1fa831c69dac6a6637a9a01179ffa984
Nanashi <ny1113548@gmail.com>
2024-08-18 13:21:43 +0530

    fixed no img

--- customChara/views.py
+++ customChara/views.py
@@ -3,4 +3,8 @@ from utils.scrape import img
 
 def onePiece(request):
     list_of_img = img("one piece")
-    return render(request, 'customChara.html',{"imgs": list_of_img})
+    if len(list_of_img) < 10:
+        list_of_img = img("one piece")
+
+    else :
+        return render(request, 'customChara.html',{"imgs": list_of_img})
